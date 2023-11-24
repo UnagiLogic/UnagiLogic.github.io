9272e2c1824df2864ec19d31a37440e1e45153de
UnagiLogic <unagilogic@gmail.com>
2023-11-24 01:12:55 -0700

    Styling CSS some more

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
-// Variables that don't need to change go here
+// Const that don't need to change go here
 
-
-// Variable that need to change go here
+// Variables go here -----------------------------------
 
 // Resource Variables
-let fish = 0;
-let clay = 0;
-let mud = 0;
+let resources = {
+    fish: 0,
+    clay: 0,
+    mud: 0,
+}
 
-let fishPerSecond = 0;
-let fishPerClick = 1;
+// Purchased resource Variables
+let purchasedResources = {
+  fishPerSecond: 0,
+  fishPerClick: 1,
+}
 
 // Cost of Action Variables
 let searchActionCost = 10
 
-// Add load EventListeners here
-
-
 // Functions go here -----------------------------------
+
+// Function to update resource display in the HTML
+function updateResourceDisplay() {
+    document.getElementById("showFish").innerText = fish;
+    document.getElementById("showClay").innerText = clay;
+    document.getElementById("showMud").innerText = mud;
+}
 
 // Function that gets called when you click the clickButton
 function increment() {
-    fish += fishPerClick; // adds the number of fish per click to your total fish
-    document.getElementById("showFish").innerText = fish; // Uses the Document Object Model to change the number of fish in the HTML
+  resources.fish += fishPerClick; // adds the number of fish per click to your total fish
+  updateResourceDisplay(); // Uses the Document Object Model to change the number of fish in the HTML
 
-    if (fish >= 100000) {
-        document.getElementById("gameOver").innerText = "You win the game! for now..."
-    } 
+  if (resources.fish >= 100000) {
+    document.getElementById("gameOver").innerText = "You win the game! for now..."
+  }
 }
 
 // Function that happens every second
 setInterval(function() {
-    fish += fishPerSecond; //adds the number of fish per second to your total fish
-    document.getElementById("showFish").innerText = fish; // Use this when you want to update your HTML
-}, 1000) // Every 1000 milliseconds
+  resources.fish += purchasedResources.fishPerSecond; //adds the number of fish per second to your total fish
+  updateResourceDisplay(); // Use this when you want to update your HTML
+}, 1000); // Every 1000 milliseconds
 
 // Function that will be called when the user presses the buyActionButton
 function buySearchAction() {
-    if (fish >= searchActionCost) { //Checks if you have enough fish
-        fish -= searchActionCost; // This subtracts the fish at cost value
-        document.getElementById("showFish").innerText = fish; // Use this when you want to update your HTML
-        
-        clay++;
-        mud++;
-        document.getElementById("showClay").innerText = clay;
-        document.getElementById("showMud").innerText = mud;
-    }
+  if (resources.fish >= searchActionCost) { //Checks if you have enough fish
+    resources.fish -= searchActionCost; // This subtracts the fish at cost value
+    updateResourceDisplay(); // Use this when you want to update your HTML
+    
+    resources.clay++;
+    resources.mud++;
+    updateResourceDisplay();
+  }
 }
 
-// A function to make a button toggle the inventory in view and out of view for the player.
-function toggleInventoryContainerVisibility() {
-    var containerElement = document.getElementById('containerForInventory');
-    containerElement.classList.toggle('hidden');
+// A function to make a button toggle the visibility of a container.
+function toggleContainerVisibility(containerId) {
+  var containerElement = document.getElementById(containerId);
+  containerElement.classList.toggle('hidden');
+  if (!containerElement.classList.contains('hidden')) {
+    containerElement.classList.add('opened');
+  } else {
+    containerElement.classList.remove('opened');
   }
-  
-  window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('inventoryButton').addEventListener('click', toggleInventoryContainerVisibility);
+}
+
+// Event Listeners go here -----------------------------------
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('inventoryButton').addEventListener('click', function() {
+    toggleContainerVisibility('containerForInventory');
   });
 
-// A Toggle for the Bestiary Book
-function toggleBestiaryContainerVisibility() {
-  var containerElement2 = document.getElementById('bestiaryContainer');
-  containerElement2.classList.toggle('hidden');
-
-  if (!containerElement2.classList.contains('hidden')) {
-    containerElement2.classList.add('opened');
-  } else {
-    containerElement2.classList.remove('opened');
-  }
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('bestiaryButton').addEventListener('click', toggleBestiaryContainerVisibility);
-  console.log("Bestiary Loaded");
-});
+  document.getElementById('bestiaryButton').addEventListener('click', function() {
+    toggleContainerVisibility('bestiaryContainer');
+    console.log("Bestiary Loaded");
+  });
+});  
