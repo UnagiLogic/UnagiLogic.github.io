0f481266859fda7eeee05ed015ed0deee02d9a7e
UnagiLogic <unagilogic@gmail.com>
2023-11-29 22:29:00 -0700

    May have figured out the issue. I might have named the fish incorrectly so testing the theory out

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function increment() {
 // Then the image will float up the screen and disappear after a few seconds.
 function displayFishImage() {
   let fishImage = document.createElement("img");
-  fishImage.src = "images/fish" + Math.floor(Math.random() * 6) + ".png";
+  fishImage.src = "Images/Resources/saltwaterFish" + Math.floor(Math.random() * 6) + ".png";
   fishImage.classList.add("fishImage");
   document.getElementById("saltwaterAreaFishClickerButton").appendChild(fishImage);
   setTimeout(function() {
