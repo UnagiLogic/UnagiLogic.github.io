aed7d5dc7d445d6fddc213214f7ec2f403005dad
UnagiLogic <unagilogic@gmail.com>
2024-04-06 11:30:24 -0600

    Dynamic hiding Attempt 0.5

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,24 +12,13 @@ import { toggleDisplay, button, buttons, containers, container, container2 } fro
 //    return;
 //}
 
-// This check if buttons with .toggle-button class are clicked
-// If they are clicked, it toggles the corresponding container
-console.log(buttons); // Check if buttons is defined and is a NodeList
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        console.log('button clicked'); // Check if button is clicked
-        
-        let containerName = button.getAttribute('data-container');
-        console.log(containerName); // Check if containerName is correct
-        
-        let container = document.getElementById(containerName);
-        console.log(container); // Check if container is correct
-
-        toggleDisplay(container);
-        console.log('container toggled'); // Check if container is toggled
+document.querySelectorAll('.toggle-button').forEach(button => {
+    button.addEventListener('click', () => {
+      const container = document.getElementById(button.dataset.container);
+      container.classList.toggle('d-none'); // Toggle d-none class
     });
-});
+  });
 
 //button.addEventListener('click', function() {
 //    toggleDisplay(container);
