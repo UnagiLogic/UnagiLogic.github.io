917748882eb56daadc6290b5f5874e663fd7f324
UnagiLogic <unagilogic@gmail.com>
2024-04-06 11:05:21 -0600

    Dynamic hiding Attempt 0.2

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,5 @@
 import { sayHelloAgain } from './splash-screen.js';
-import { toggleDisplay, button, container, container2 } from './common.js';
+import { toggleDisplay, button, buttons, containers, container, container2 } from './common.js';
 //create a function that checks if gameState is in the splash screen state
 //if it is, call splashScreen
 //if it is not, do nothing
@@ -12,10 +12,20 @@ import { toggleDisplay, button, container, container2 } from './common.js';
 //    return;
 //}
 
-button.addEventListener('click', function() {
-    toggleDisplay(container);
-    toggleDisplay(container2);
+// This check if buttons with .toggle-button class are clicked
+// If they are clicked, it toggles the corresponding container
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        let containerName = button.getAttribute('data-container');
+        let container = document.getElementById(containerName);
+        toggleDisplay(container);
+    });
 });
+
+//button.addEventListener('click', function() {
+//    toggleDisplay(container);
+//    toggleDisplay(container2);
+//});
 
 
 // create a function that says hello on the screen
--- Scripts/common.js
+++ Scripts/common.js
@@ -18,14 +18,18 @@
 export let button = document.getElementById('toggleButton');
 export let container = document.getElementById('splash-screen');
 export let container2 = document.getElementById('inventory-menu');
-//This is an array of buttons
-export let buttons = document.querySelectorAll('button');
+
+//This is an array of clickable buttons that can have their display toggled
+export let buttons = document.querySelectorAll('.toggle-button');
+
+//This is an array of containers that can have their display toggled
+export let containers = document.querySelectorAll('.toggle-container');
+
 
 // Functions go here -----------------------------------
 
 // What do I want the function to do?
 
-// this function checks in an array of buttons wher
 
 // What do I want the fucntion to do?
 // Create a function called toggleDisplay
